6c9773945fc9e46c684efcaa4515c0e7691601c3
lilamaris <lilamaris.dev@gmail.com>
2025-08-25 19:29:05 +0900

    add: remark & rehype plugins for markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,44 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
 import swup from "@swup/astro";
-
 import icon from "astro-icon";
+
+import rehypeSlug from "rehype-slug";
+import rehypeAutoLinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "always",
   vite: {
     plugins: [tailwindcss()],
+  },
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutoLinkHeadings,
+        {
+          behavior: "append",
+          properties: {
+            className: ["anchor"],
+          },
+          content: {
+            type: "element",
+            tagName: "span",
+            properties: {
+              className: ["anchor-icon"],
+              "data-pagefind-ignore": true,
+            },
+            children: [
+              {
+                type: "text",
+                value: "#",
+              },
+            ],
+          },
+        },
+      ],
+    ],
   },
   integrations: [
     svelte(),
@@ -22,6 +52,7 @@ export default defineConfig({
     }),
     swup({
       globalInstance: true,
+      smoothScrolling: true,
     }),
   ],
 });
