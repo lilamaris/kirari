c557d9b74e5bbf7aa95e2afd2ddef1a046c81f2c
lilamaris <lilamaris.dev@gmail.com>
2025-08-25 16:45:52 +0900

    fix: hue handle style

--- src/components/HueController.svelte
+++ src/components/HueController.svelte
@@ -4,8 +4,14 @@
 
   let hue = $state(getHue());
 
+  let timer: NodeJS.Timeout;
   $effect(() => {
-    setHue(hue);
+    ((hue: number) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        setHue(hue);
+      }, 50);
+    })(hue);
   });
 </script>
 
@@ -19,60 +25,14 @@
   </button>
 
   <div class="card w-80" data-float-menu-role="content">
+    {hue}
     <input
+      class="hueHandle"
       max="360"
       min="0"
       step="5"
-      id="hueHandle"
       type="range"
       bind:value={hue}
     />
   </div>
 </div>
-
-<style>
-  #hueHandle {
-    border-radius: 0.125rem;
-    padding: 0 5px 0 5px;
-    appearance: none;
-    height: 1.5rem;
-    background-image: var(--rainbow);
-    width: 100%;
-
-    &::-moz-range-thumb {
-      appearance: none;
-      width: 0.5rem;
-      height: 1rem;
-      border: none;
-      border-radius: 0.125rem;
-      background: rgba(255, 255, 255, 0.7);
-      box-shadow: none;
-    }
-
-    &::-moz-range-thumb:hover {
-      background-color: rgba(255, 255, 255, 0.8);
-    }
-
-    &::-moz-range-thumb:active {
-      background-color: rgba(255, 255, 255, 0.6);
-    }
-
-    &::-webkit-slider-thumb {
-      appearance: none;
-      width: 0.5rem;
-      height: 1rem;
-      border: none;
-      border-radius: 0.125rem;
-      background: rgba(255, 255, 255, 0.7);
-      box-shadow: none;
-    }
-
-    &::-webkit-slider-thumb:hover {
-      background-color: rgba(255, 255, 255, 0.8);
-    }
-
-    &::-webkit-slider-thumb:active {
-      background-color: rgba(255, 255, 255, 0.6);
-    }
-  }
-</style>
