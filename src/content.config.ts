05789a3b32af230a034b3acab35f06aa50dad2dc
lilamaris <lilamaris.dev@gmail.com>
2025-08-26 18:45:06 +0900

    refactor: post load

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,24 @@
 import { glob } from "astro/loaders";
-import { defineCollection } from "astro:content";
-import { postSchema } from "./lib/content-util";
+import { z, defineCollection } from "astro:content";
 
 const post = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/post" }),
-  schema: postSchema,
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      slug: z.string(),
+      series: z.string().optional(),
+      seriesOrder: z.number().optional(),
+      tags: z.array(z.string()).default([]),
+      published: z.coerce.date(),
+      draft: z.boolean().default(false),
+      image: image().optional(),
+
+      // NOTE: Refer to post located before and after the relevant post
+      // Use this field as an ID to retrieve data from collection using 'getEntry' in 'astro:content'
+      previousPostSlug: z.string().optional(),
+      nextPostSlug: z.string().optional(),
+    }),
 });
 
 export const collections = { post };
