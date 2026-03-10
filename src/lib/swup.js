b395c61efd7c32c50f33bb959a996a49272c115f
lilamaris <lilamaris.dev@gmail.com>
2026-03-11 00:44:42 +0900

    fix: handle swup for RSS and feed endpoints
    swup was intercepting /rss.xml as an internal page visit. Because the RSS response does not include the #swup-target container, the transition failed during content replacement and left html stuck with swup state classes. This excludes the RSS button explicitly and ignores non-HTML feed/meta endpoints in swup.

--- src/lib/swup.js
+++ src/lib/swup.js
@@ -1,6 +1,18 @@
 import Swup from "swup";
 import { splitUrl } from "./utils";
 
+const NON_HTML_ENDPOINTS = new Set([
+  "/rss.xml",
+  "/sitemap.xml",
+  "/robots.txt",
+  "/manifest.webmanifest",
+]);
+
+const isNonHtmlEndpoint = (url) => {
+  const pathname = new URL(url, window.location.origin).pathname;
+  return NON_HTML_ENDPOINTS.has(pathname);
+};
+
 const setBaseUrlToDocument = () => {
   const path = window.location.pathname;
   const parts = splitUrl(path, { includeRoot: false });
@@ -11,6 +23,8 @@ const setBaseUrlToDocument = () => {
 const swup = new Swup({
   containers: ["#swup-target"],
   animationSelector: '[class*="transition-swup-"]',
+  ignoreVisit: (url, { el } = {}) =>
+    !!el?.closest("[data-no-swup]") || isNonHtmlEndpoint(url),
 });
 
 swup.hooks.on("animation:out:start", () => {
