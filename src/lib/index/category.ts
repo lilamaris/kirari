8e4d786a4d510384714f23149e4791cb9fa013d7
lilamaris <lilamaris.dev@gmail.com>
2026-06-01 07:28:21 +0900

    fix: add direct post count & subtree aggregated post count field

--- src/lib/index/category.ts
+++ src/lib/index/category.ts
@@ -5,8 +5,11 @@ export type CategoryName = string;
 export interface CategoryNode {
   name: CategoryName;
   categories: CategoryName[];
+  depth: number;
   children: Record<CategoryName, CategoryNode>;
   directPostRefs: CategorizedPost[];
+  directPostCount: number;
+  subtreePostCount: number;
 }
 
 export interface CategorizedPost {
@@ -27,13 +30,23 @@ export const getCategoryIndex = async (): Promise<CategoryTree> => {
   return _cache;
 };
 
+export const getFlatten = (node: CategoryNode): CategoryNode[] => {
+  const result: CategoryNode[] = [node];
+  for (const child of Object.values(node.children)) {
+    result.push(...getFlatten(child));
+  }
+  return result;
+};
+
 const buildCategoryTree = (posts: Post[]): CategoryTree => {
-  const root = createNode("", ["/"]);
+  const root = createNode("All", ["/"], 0);
 
   for (const post of posts) {
     const categorizedPost = categorize(post);
     appendPostToTree(root, categorizedPost);
   }
+
+  countPost(root);
   return { root };
 };
 
@@ -44,7 +57,11 @@ const appendPostToTree = (root: CategoryNode, post: CategorizedPost): void => {
   for (const category of post.categories) {
     segments.push(category);
 
-    current.children[category] ??= createNode(category, segments);
+    current.children[category] ??= createNode(
+      category,
+      segments,
+      current.depth + 1,
+    );
 
     current = current.children[category];
   }
@@ -52,17 +69,34 @@ const appendPostToTree = (root: CategoryNode, post: CategorizedPost): void => {
   current.directPostRefs.push(post);
 };
 
-const createNode = (name: CategoryName, categories: CategoryName[]) => ({
+const countPost = (node: CategoryNode): number => {
+  node.directPostCount = node.directPostRefs.length;
+  let count = node.directPostCount;
+  for (const child of Object.values(node.children)) {
+    count += countPost(child);
+  }
+  node.subtreePostCount = count;
+  return count;
+};
+
+const createNode = (
+  name: CategoryName,
+  categories: CategoryName[],
+  depth: number,
+) => ({
   name,
   categories,
+  depth,
   children: {},
   directPostRefs: [],
+  directPostCount: 0,
+  subtreePostCount: 0,
 });
 
 const categorize = (post: Post): CategorizedPost => {
   const contentPath = post.data.contentPath;
   if (!contentPath)
-    throw Error(`post filePath must not be undefined. id=${post.id}`);
+    throw new Error(`post filePath must not be undefined. id=${post.id}`);
 
   const paths = getCategoriesFromFilePath(contentPath);
 
