4f362c0cf8d44f4401afde95b5f2968c9921440c
lilamaris <lilamaris.dev@gmail.com>
2026-06-01 03:40:40 +0900

    feat: add published date index

---
+++ src/lib/index/published.ts
@@ -0,0 +1,77 @@
+import type { Post } from "@/types";
+import { getCollection } from "astro:content";
+import { compareDate, objectKeys } from "../utils";
+
+export type Year = string;
+export type Month = number;
+
+export interface PublishedPost {
+  id: string;
+  published: Date;
+}
+
+export interface MonthEntry {
+  month: number;
+  posts: PublishedPost[];
+}
+
+export type PublishedIndex = Record<Year, MonthEntry[]>;
+
+let _cache: PublishedIndex | undefined;
+
+export const getPublishedIndex = async (): Promise<PublishedIndex> => {
+  if (_cache) return _cache;
+  const posts = await getCollection("posts");
+  _cache = buildPublishedIndex(posts);
+  return _cache;
+};
+
+const buildPublishedIndex = (posts: Post[]): PublishedIndex => {
+  const index: PublishedIndex = {};
+
+  for (const post of posts) {
+    const normalizedPost = normalize(post);
+    appendPostToIndex(index, normalizedPost);
+  }
+
+  for (const year of objectKeys(index)) {
+    index[year].sort((a, b) => a.month - b.month);
+    for (const entry of index[year]) {
+      entry.posts.sort((a, b) => compareDate(a.published, b.published, "desc"));
+    }
+  }
+
+  return index;
+};
+
+interface YearAndMonth {
+  year: Year;
+  month: Month;
+}
+
+const appendPostToIndex = (
+  index: PublishedIndex,
+  post: PublishedPost,
+): void => {
+  const { year, month } = getYearAndMonth(post.published);
+
+  index[year] ??= [];
+
+  const monthEntry = (index[year][month] ??= createMonthEntry(month));
+  monthEntry.posts.push(post);
+};
+
+const getYearAndMonth = (date: Date): YearAndMonth => ({
+  year: String(date.getFullYear()),
+  month: date.getMonth() + 1,
+});
+
+const createMonthEntry = (month: Month): MonthEntry => ({
+  month,
+  posts: [],
+});
+
+const normalize = (post: Post): PublishedPost => ({
+  id: post.id,
+  published: post.data.published,
+});
